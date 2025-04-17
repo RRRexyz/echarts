1a4b8c0593c34b29c8f8a559dadf2abf93241a80
Rex <2840722773@qq.com>
2025-04-17 22:03:29 +0800

    feat(core): 添加项目结构和旅行足迹图功能

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from echarts!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ travel.py
@@ -0,0 +1,42 @@
+from pyecharts import options as opts
+from pyecharts.charts import Map
+from pyecharts.commons.utils import JsCode
+
+cities = [
+    '无锡市',
+    '常州市',
+    '徐州市',
+    '济南市',
+    '淄博市',
+    '泰安市',
+    '青岛市',
+    '威海市',
+    '烟台市',
+    '北京市',
+    '天津市',
+    '沈阳市',
+    '哈尔滨市'
+]
+
+values = [1] * len(cities)
+
+c = (
+    Map()
+    .add(series_name="旅行足迹", 
+        data_pair=[list(z) for z in zip(cities, values)],
+        maptype="china-cities",
+        label_opts=opts.LabelOpts(is_show=True,
+            position="insideBottom",
+            formatter=JsCode(
+                f"function(params) {{ if ({cities}.includes(params.name)) {{ return params.name; }} else {{ return ''; }} }}") 
+        ),
+        center=[121, 39],
+        zoom=3.5
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="旅行足迹图"),
+        legend_opts=opts.LegendOpts(is_show=False),
+        visualmap_opts=opts.VisualMapOpts(is_show=False)
+    )
+    .render("旅行足迹图.html")
+)
